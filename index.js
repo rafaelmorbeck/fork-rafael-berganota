360591b5132210ee775e47d0fd1493bdf5373973
jupitters <sampaiobale@gmail.com>
2025-06-24 23:49:27 -0300

    rotas no index js

--- index.js
+++ index.js
@@ -7,10 +7,27 @@ PORT=8080
 app.engine('html', mustacheExpress());
 app.set('view-engine', 'html');
 app.set('views', __dirname + '/views');
+app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-    res.send("oi");
+    res.render('index.html');
+})
+
+app.get('/cadastro', (req, res) => {
+    res.render('cadastro.html');
+})
+
+app.get('/inicio', (req, res) => {
+    res.render('inicio.html');
+})
+
+app.get('/perfil', (req, res) => {
+    res.render('profile.html');
+})
+
+app.get('/teste', (req, res) => {
+    res.render('teste.html');
 })
 
 
