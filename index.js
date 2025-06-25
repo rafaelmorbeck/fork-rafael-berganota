73227fb959b4f6ecccc620bd14cc5d53f7ae5cc8
jupitters <sampaiobale@gmail.com>
2025-06-24 23:18:40 -0300

    using mustache

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mustacheExpress = require('mustache-express');
+
 const app = express();
 PORT=8080
+
+app.engine('html', mustacheExpress());
+app.set('view-engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.send("oi");
